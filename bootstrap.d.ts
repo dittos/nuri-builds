9431abf9f2bb69d48cc72f12c885de163b92e719
ditto <dittos@gmail.com>
2019-03-17 10:34:59 +0900

    Fix name collision

--- bootstrap.d.ts
+++ bootstrap.d.ts
@@ -1,4 +1,4 @@
-export declare const containerElementId = "__nuri";
+export declare const containerElementId = "__nuri_root";
 export declare const globalVariableName = "__nuri";
 export declare const preludeScript: string;
 export declare function wrapHTML(content: string): string;
--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.containerElementId = '__nuri';
+exports.containerElementId = '__nuri_root';
 exports.globalVariableName = '__nuri';
 exports.preludeScript = "(function(w,n){w[n]=w[n]||function(p){w[n].preloadData=p}})(window," + JSON.stringify(exports.globalVariableName) + ");";
 function wrapHTML(content) {
